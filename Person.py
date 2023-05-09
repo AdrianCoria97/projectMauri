6679c408a6892f716c2c553f14e279464bb8420d
AdrianCoria97 <95306064+AdrianCoria97@users.noreply.github.com>
2023-05-09 20:14:40 -0300

    aniadiendo funcionalidades (#2)

--- Person.py
+++ Person.py
@@ -6,23 +6,28 @@ class Person:
     city = str
     identification_number = int
     __age = int
+    professional_license: bool
 
-    def __init__(self, name, city, identification_number, age):
+    def __init__(self, name, city, identification_number, age, professional_license):
         self.city = city
         self.__name = name
         self.identification_number = identification_number
         self.__age = age
+        self.professional_license = professional_license
 
     # getter
     def return_age(self) -> int:
         return self.__age
 
-    # principios de encasulamiento(este metodo no puede ser llamado desde otra clase) __
+    # principios de encasulamiento (este metodo no puede ser llamado desde otra clase) __
     def return_name(self):
         return self.__name
 
     def return_city(self):
         return self.city
+
+    def has_license(self):
+        return self.professional_license
 
     # de esta manera se puede desencapsular
     # def test(self):
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from Car import Car
+from Car import Car, Ambulance
 from Person import Person
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    """
+    #
     name = input("name of person: ")
     city = input("city of person: ")
     identification = input("number of the identification: ")
     age = input("your age: ")
+    professional_license = input("Do you have a professional license?")
     color_car = input("Color car is: ")
-
+    
+    # >>>>>>>OBJETOS<<<<<<<<<<< 
     # parametros posicionales y parametros nombrados
-    doctor = Person(name=name, city=city, identification_number=identification, age=age)
+    doctor = Person(name=name, city=city, identification_number=identification, age=age, professional_license=professional_license)
     fiat = Car(color=color_car, owner=doctor)
-    new_proprietary = Person("Adrian", "Junin", 511951, 18)
+    new_proprietary = Person("Adrian", "Junin", 511951, 18, True)
 
     fiat.print_attributes()
     fiat.transfer(new_proprietary)
+    
+    """
+    # >>>>>>>>OBJETOS DE HERENCIA<<<<<<<<<<
 
-    print(doctor.return_name())
-    print(doctor.return_city())
-    print("Hola")
+    conductor = Person(name="Juan", city="NY", identification_number=15406423, age=23, professional_license=False)
+    ambulance = Ambulance(color="red and white", owner=conductor)
 
+    # metodos de Car (sobre escritura y sobrecarga con super)
+    ambulance.print_attributes()
+    # metodos de Ambulance
+    print(ambulance.ambulance_type("van"))
 
+    new_proprietary_ambulance = Person(name="Mauri", city="San Martin", identification_number=2634557788, age=18, professional_license=True)
+    new_car = Ambulance(color="red and white", owner=new_proprietary_ambulance)
+    new_car.transfer(new_proprietary_ambulance)
